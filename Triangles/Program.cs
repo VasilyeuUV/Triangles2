b3d15e9472bd3462ab66bf373b5c59037706886f
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-07-27 23:32:43 +0300

    app: prevent application from being double-launched

--- Triangles/Program.cs
+++ Triangles/Program.cs
@@ -4,6 +4,7 @@ namespace Triangles
 {
     internal static class Program
     {
+        private static string appGuid = "3ae4924c-bfe8-402c-a879-7b8e10013751";          // - appGuid для Mutex-a
         private static ApplicationBootstrapper? _bootstrapper;
 
 
@@ -13,24 +14,38 @@ namespace Triangles
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            //Mutex для запрета повторного запуска приложения
+            using (Mutex mutex = new Mutex(false, @"Global\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Внимание! Программа уже запущена. Запуск копии заблокирован", "Программа сообщает:");
+                    return;
+                }
 
-            _bootstrapper = new ApplicationBootstrapper();
+                GC.Collect();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-            // Add the event handler for handling UI thread exceptions to the event.
-            Application.ThreadException += new ThreadExceptionEventHandler(Form_UIThreadException);
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
 
-            // Set the unhandled exception mode to force all Windows Forms errors
-            // to go through our handler.
-            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+                _bootstrapper = new ApplicationBootstrapper();
 
-            // Add the event handler for handling non-UI thread exceptions to the event. 
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(OnUnhandledExceptionRaised);
+                // Add the event handler for handling UI thread exceptions to the event.
+                Application.ThreadException += new ThreadExceptionEventHandler(Form_UIThreadException);
 
-            var MainForm = _bootstrapper.CreateApplication();
-            Application.Run(MainForm.Run());
+                // Set the unhandled exception mode to force all Windows Forms errors
+                // to go through our handler.
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+                // Add the event handler for handling non-UI thread exceptions to the event. 
+                AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(OnUnhandledExceptionRaised);
+
+                var MainForm = _bootstrapper.CreateApplication();
+                Application.Run(MainForm.Run());
+            }
         }
 
         private static void Form_UIThreadException(object sender, ThreadExceptionEventArgs e)
